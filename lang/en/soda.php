b9b59108827245fd90805be2dacdfdb07d78f1cb
Menno de Ridder <menno@menno-laptop.(none)>
2011-10-31 11:49:44 +0100

    Added language folder, which is required for the installation of new modules in moodle 2.1

---
+++ lang/en/soda.php
@@ -0,0 +1,13 @@
+<?php
+defined('MOODLE_INTERNAL') || die();
+
+$string['modulename'] = 'soda';
+$string['modulenameplural'] = 'soda';
+$string['modulename_help'] = 'This module is used to create and edit new modules easily. Please hide this module from view by users.';
+$string['newmodulefieldset'] = 'Custom example fieldset';
+$string['newmodulename'] = 'soda name';
+$string['newmodulename_help'] = 'This module is used to create and edit new modules easily. Please hide this module from view by users.';
+$string['newmodule'] = 'soda';
+$string['pluginadministration'] = 'soda administration';
+$string['pluginname'] = 'soda';
+?>
