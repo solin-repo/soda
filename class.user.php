b7477aaf220359fba14715f35767b04802cca4d9
Menno de Ridder <menno@menno-laptop.(none)>
2011-11-24 12:55:00 +0100

    Added commentary and string to hash function to create_password

--- class.user.php
+++ class.user.php
@@ -1,6 +1,16 @@
 <?php
 class user extends model {
 	
+    /* Menno de Ridder, 24-11-2011
+     *
+     * This function creates a random password of
+     * by default 8 characters.
+     *
+     * It returns an array with both the unhashed
+     * password as the hashed password.
+     * @param  int  $length  Length of the password (default = 8)
+     * @return  array(password => hashed_password)
+     */
 	function create_password($length = 8) {
 
 		$allowed_characters = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHJKLMNOPQRSTUVWXYZ";
@@ -9,12 +19,13 @@ class user extends model {
 		for($i = 0; $i < $length; $i ++)
 		{
 			$char = substr($allowed_characters, mt_rand(0, strlen($allowed_characters)-1), 1);
-			if (!strstr($password, $char)) {
-				$password .= $char;
-			} else {
-				$i -= 1;
+            if (!strstr($password, $char)) {
+                $password .= $char;
+            } else {
+                $i -= 1;
+            }
 		}
-		return $password;
+		return array($password=>hash_internal_user_password($password));
 	}
 }
 ?>
