71ea433770cdce84f8c5f3f024e1a86b55fab151
Menno de Ridder <menno@menno-laptop.(none)>
2011-12-27 11:42:58 +0100

    Added lang folder to soda 1.9

---
+++ lang/en_utf8/soda.php
@@ -0,0 +1,12 @@
+<?php
+
+$string['newmodule'] = 'Soda';
+
+$string['modulename'] = 'Soda';
+$string['modulenameplural'] = 'Sodas';
+
+$string['newmodulefieldset'] = 'Soda Fieldset';
+$string['newmoduleintro'] = 'This module should be set as invisible!';
+$string['newmodulename'] = 'Soda';
+
+?>
