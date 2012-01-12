ad9694eae6aa1b949cd8eb935494c60b3f30da4c
Onno Schuit <o.schuit@solin.nl>
2012-01-12 10:20:13 +0100

    Changed moodle19 branch into local plugin

--- lang/en_utf8/soda.php
+++ lang/en_utf8/soda.php
@@ -1,12 +1,38 @@
 <?php
 
-$string['newmodule'] = 'Soda';
+/////////////////////////////////////////////////////////////////////////////
+//                                                                         //
+// NOTICE OF COPYRIGHT                                                     //
+//                                                                         //
+// Moodle - Soda Module Constructor and MVC Framework                      //
+//                                                                         //
+// Copyright (C) 2011 Solin - www.solin.eu                                 //
+//                                                                         //
+//                                                                         //
+// Programming and development:                                            //
+//     Onno Schuit (o.schuit[atoraround]solin.nl)                          //
+//                                                                         //
+// For bugs, suggestions, etc. contact:                                    //
+//     Onno Schuit (o.schuit[atoraround]solin.nl)                          //
+//                                                                         //
+// This program is free software; you can redistribute it and/or modify    //
+// it under the terms of the GNU General Public License as published by    //
+// the Free Software Foundation; either version 2 of the License, or       //
+// (at your option) any later version.                                     //
+//                                                                         //
+// This program is distributed in the hope that it will be useful,         //
+// but WITHOUT ANY WARRANTY; without even the implied warranty of          //
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           //
+// GNU General Public License for more details:                            //
+//                                                                         //
+//          http://www.gnu.org/copyleft/gpl.html                           //
+//                                                                         //
+/////////////////////////////////////////////////////////////////////////////
 
-$string['modulename'] = 'Soda';
-$string['modulenameplural'] = 'Sodas';
-
-$string['newmodulefieldset'] = 'Soda Fieldset';
-$string['newmoduleintro'] = 'This module should be set as invisible!';
-$string['newmodulename'] = 'Soda';
+/**
+ * File required by Moodle.
+ * @package Soda
+ */
+$string['pluginname'] = 'Soda';
 
 ?>
--- lib.js
+++ lib.js
@@ -1,3 +1,32 @@
+/////////////////////////////////////////////////////////////////////////////
+//                                                                         //
+// NOTICE OF COPYRIGHT                                                     //
+//                                                                         //
+// Moodle - Soda Module Constructor and MVC Framework                      //
+//                                                                         //
+// Copyright (C) 2011 Solin - www.solin.eu                                 //
+//                                                                         //
+//                                                                         //
+// Programming and development:                                            //
+//     Onno Schuit (o.schuit[atoraround]solin.nl)                          //
+//                                                                         //
+// For bugs, suggestions, etc. contact:                                    //
+//     Onno Schuit (o.schuit[atoraround]solin.nl)                          //
+//                                                                         //
+// This program is free software; you can redistribute it and/or modify    //
+// it under the terms of the GNU General Public License as published by    //
+// the Free Software Foundation; either version 2 of the License, or       //
+// (at your option) any later version.                                     //
+//                                                                         //
+// This program is distributed in the hope that it will be useful,         //
+// but WITHOUT ANY WARRANTY; without even the implied warranty of          //
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           //
+// GNU General Public License for more details:                            //
+//                                                                         //
+//          http://www.gnu.org/copyleft/gpl.html                           //
+//                                                                         //
+/////////////////////////////////////////////////////////////////////////////
+
 function post_to_url(path, params, method) {
     method = method || "post"; // Set method to post by default, if not specified.
 
