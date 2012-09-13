23837b620f03e7b8a2de01da9a1e5fb56e4a93f0
Onno Schuit <o.schuit@solin.nl>
2012-09-13 11:35:46 +0200

    Added 'supports' function as a wrapper function for x_supports (where x is the module's name)

--- class.soda.php
+++ class.soda.php
@@ -349,6 +349,31 @@ class soda {
      * Please overwrite them in your own module's class if you need deviant behavior.
      ***************************************************************************************************************/
 
+
+    /**
+     * List of features supported in your module
+     * @param string $feature FEATURE_xx constant for requested feature
+     * @return mixed True if module supports feature, false if not, null if doesn't know
+     */
+    static function supports($feature) {
+        switch($feature) {
+            /*
+            case FEATURE_MOD_ARCHETYPE:           return MOD_ARCHETYPE_RESOURCE;
+            case FEATURE_GROUPS:                  return false;
+            case FEATURE_GROUPINGS:               return false;
+            case FEATURE_GROUPMEMBERSONLY:        return true;
+            case FEATURE_MOD_INTRO:               return true;
+            case FEATURE_COMPLETION_TRACKS_VIEWS: return true;
+            case FEATURE_GRADE_HAS_GRADE:         return false;
+            case FEATURE_GRADE_OUTCOMES:          return false;
+            case FEATURE_BACKUP_MOODLE2:          return true;
+            case FEATURE_SHOW_DESCRIPTION:        return true;
+            */
+            default: return null;
+        }
+    } // function supports
+
+
     static function add_instance($mod_instance) {
         global $DB;
 
