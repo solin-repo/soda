fe3e5ebf652706b5709fffaa771568aaf3bd6181
onno-schuit <o.schuit@solin.nl>
2013-04-03 11:45:55 +0200

    minor changes

--- class.student.php
+++ class.student.php
@@ -42,19 +42,31 @@ class student extends user {
      */
     static function load_by_course($course_id, $where = false) {
         $where_clause = ($where) ? " AND $where " : '';
+        // 'NOT EXISTS' subselect is necessary because a teacher may also have a student role (even in the same course)
         return student::base_load(
             "SELECT u.*
              FROM {user} AS u
              INNER JOIN {role_assignments} ra ON ra.userid = u.id
-             INNER JOIN {context} c ON c.id = ra.contextid AND c.contextlevel = '50' AND c.instanceid = :course_id
+             INNER JOIN {context} c ON c.id = ra.contextid AND c.contextlevel = '50' AND c.instanceid = ?
              INNER JOIN {role} r ON ra.roleid = r.id
-             WHERE r.shortname = 'student' AND NOT EXISTS 
-                (SELECT ra2.userid 
-                 FROM {role_assignments} AS ra2, {role} AS r2 
-                 WHERE ra2.userid = u.id AND ra2.roleid = r2.id AND r2.shortname NOT LIKE 'student')
+             WHERE r.shortname = 'student' AND u.deleted = 0 AND NOT EXISTS 
+                 (SELECT u2.*
+                  FROM {user} AS u2
+                  INNER JOIN {role_assignments} ra2 ON ra2.userid = u2.id
+                  INNER JOIN {context} c2 ON c2.id = ra2.contextid AND c2.contextlevel = '50' AND c2.instanceid = ?
+                  INNER JOIN {role} r2 ON ra2.roleid = r2.id
+                  WHERE r2.shortname NOT LIKE 'student' AND u2.id = u.id)
              $where_clause",
-            array("course_id" => $course_id) ); 
+            array($course_id, $course_id ) ); 
+        /*
+                (SELECT ra2.userid 
+                 FROM {role_assignments} AS ra2, {role} AS r2,  
+                 WHERE ra2.userid = u.id AND ra2.roleid = r2.id AND r2.shortname NOT LIKE 'student')
+         */
     } // function load_by_course
+
+
+
 
 } // class student 
 	
