27de1f990b84fe70a488360f844c374ae359f269
onno-schuit <o.schuit@solin.nl>
2013-04-11 17:00:53 +0200

    Included access.php file in template for new modules

---
+++ template/db/access.php
@@ -0,0 +1,19 @@
+<?php
+// FILENAME: mod/template/access.php
+
+defined('MOODLE_INTERNAL') || die();
+
+$capabilities = array(
+    'mod/template:addinstance' => array(
+        'riskbitmask' => RISK_XSS,
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_COURSE,
+        'archetypes' => array(
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW
+        ),
+        'clonepermissionsfrom' => 'moodle/course:manageactivities'
+    )
+);
+
+?>
