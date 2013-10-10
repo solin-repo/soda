cf45e0df7484d37aa27896991e8000e9e4f34a7c
Onno Schuit <o.schuit@solin.nl>
2013-10-10 15:07:12 +0200

    Create script now automatically inserts a version date and number

--- create.php
+++ create.php
@@ -11,11 +11,13 @@ $parsed_args = soda::arguments($argv);
 if (!count($parsed_args['arguments']) && !count($parsed_args['commands']) ) exit("Please specify a module name\n");
 $mod_name = (count($parsed_args['arguments']) ) ?  $parsed_args['arguments'][0] : $parsed_args['commands'][0];
 $dir_dest = "../../mod/$mod_name";
+$date_version = date("Ymd") . "00";
 if ( shell_exec( " mkdir $dir_dest 2>&1") == null ) {
     echo shell_exec(" cp -r -a template/* $dir_dest 2>&1 " );
     echo shell_exec(" find $dir_dest -iname '*template*' -exec rename 's/template/$mod_name/i' {} + ");
     shell_exec(" find $dir_dest -exec sed -i -e 's/template/$mod_name/g' {} \; 2>&1 ");
     shell_exec(" find $dir_dest -exec sed -i -e 's/Template/" . ucfirst($mod_name) . "/g' {} \; 2>&1 ");
+    shell_exec(" find $dir_dest -exec sed -i -e 's/DATE_VERSION/$date_version/g' {} \; 2>&1 ");
 }
 if ( !empty($parsed_args['options']['skeleton']) ) {
     $model_name = $parsed_args['options']['skeleton'];
--- template/version.php
+++ template/version.php
@@ -2,8 +2,8 @@
   // example contents for version.php
   defined('MOODLE_INTERNAL') || die();
 
-  $module->version   = 2012010400;       // The current module version (Date: YYYYMMDDXX)
+  $module->version   = DATE_VERSION;     // The current module version (Date: YYYYMMDDXX)
   $module->requires  = 2011112900;       // Requires this Moodle version
-  $module->component = 'mod_template';       // Full name of the plugin (used for diagnostics)
+  $module->component = 'mod_template';   // Full name of the plugin (used for diagnostics)
   $module->cron      = 0;
 ?>
